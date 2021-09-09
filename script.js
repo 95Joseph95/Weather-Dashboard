42e2173b59b289e31e06a34ab7b6da37f812252b
95Joseph95 <83739840+95Joseph95@users.noreply.github.com>
2021-09-08 23:45:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+var button = document.getElementById('button')
+var input = document.getElementById('input')
+
+button.addEventListener("click", function(){
+    var cityName = input.value
+    console.log(cityName)
+    cityWeather(cityName)
+
+})
+ function cityWeather(userWeather){
+    var weatherfetch = "https://api.openweathermap.org/data/2.5/weather?q="+ userWeather +"&appid=cd4c15d7efd7d6e6bfe238be28a6494d&units=imperial"
+    fetch(weatherfetch)
+    .then(response =>{
+        return response.json()
+    })
+    .then(info =>{
+        var weatherInfo = info.main.temp
+        console.log(weatherInfo)
+    })
+
+ }
